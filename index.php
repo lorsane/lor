5d3328bf9d36ff7b20b77d8333defb69996ceb13
lorsane <61491549+lorsane@users.noreply.github.com>
2020-02-26 13:56:43 +0700

    1
    1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<head>
+<script src="https://aslika.com/tracker.js"></script>
+  <meta charset="utf-8" />
+  <title></title>
+  
+</head>
+<body>
+ <div class="stepform_qKIxpul rnd_203700243"></div>
+<script>
+    (function(s, t, e, p, f, o, r, m) {
+        s[t] = s[t] || {};
+        s[t][203700243] = {
+            id: "qKIxpul",
+            rnd: 203700243
+        };
+        e.async = true;
+        e.src = p + f;
+        document[m](o)[r](e)
+    }(window,"stepFORM_params",document.createElement("script"),document.location.protocol==="https:"?"https:":"http:","//app.stepform.io/api.js?id=qKIxpul","head","appendChild","querySelector"));
+</script>  
+</body>
+</html>
